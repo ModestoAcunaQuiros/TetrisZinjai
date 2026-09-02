75d95249339822d783109fe8b0b570764179cc2d
ModestoAcunaQuiros <minor.acuna.quiros@est.una.ac.cr>
2026-09-02 10:41:19 -0600

    Feat: la cola de los eventos durante el juego

---
+++ ColaEventos.cpp
@@ -0,0 +1,51 @@
+#include "ColaEventos.h"
+
+void inicializarColaEventos(ColaEventos* cola){
+	cola->frente = nullptr;
+	cola->cantidad = 0;
+}
+void destruirColaDeEventos(ColaEventos* cola){
+	NodoEvento* actual = cola->frente;
+	while(actual != nullptr){
+		NodoEvento* siguiente = actual->siguiente;
+		delete actual;
+		actual = siguiente;
+	}
+	cola->frente = nullptr;
+	cola->cantidad = 0;
+}
+bool eventosVacio(ColaEventos* cola){
+	return cola->cantidad = 0;
+}
+void programarEvento(ColaEventos* cola, Evento evento){
+	NodoEvento* nuevo = new NodoEvento;
+	nuevo->dato = evento;
+	if(cola->frente == nullptr || evento.momentoEvento < cola->frente->dato.momentoEvento){
+		nuevo->siguiente = cola->frente;
+		cola->cantidad++;
+		return;
+	}
+	NodoEvento* actual = cola->frente;
+	while(actual->siguiente != nullptr && actual->siguiente->dato.momentoEvento <= evento.momentoEvento){
+		actual = actual->siguiente;
+	}
+	nuevo->siguiente = actual->siguiente;
+	actual->siguiente = nuevo;
+	cola->cantidad++;
+}
+	
+bool eventolisto(ColaEventos* cola, float tiempoActual){
+	if(cola->frente == nullptr){
+		return false;
+	}
+	return cola->frente->dato.momentoEvento <= tiempoActual;
+}
+	
+Evento extraeEvento(ColaEventos* cola){
+	NodoEvento* antiguoFrente = cola->frente;
+	Evento resultado = antiguoFrente ->dato;
+	cola->frente = antiguoFrente->siguiente;
+	delete antiguoFrente;
+	cola->cantidad--;
+	return resultado;
+}
---
+++ ColaEventos.h
@@ -0,0 +1,35 @@
+#ifndef COLAEVENTOS_H
+#define COLAEVENTOS_H
+
+enum TipoDeEventos{
+	aumentarVelocidad,
+	piezaBomba,
+	congelarControl
+};
+
+struct Evento{
+	float momentoEvento;
+	TipoDeEventos evento;
+};
+
+struct NodoEvento{
+	Evento dato;
+	NodoEvento* siguiente;
+};
+
+struct ColaEventos{
+	int cantidad;
+	NodoEvento* frente;
+};
+
+void inicializarColaEventos(ColaEventos* cola);
+void destruirColaDeEventos(ColaEventos* cola);
+
+void programarEvento(ColaEventos* cola, Evento evento);
+bool eventosVacio(ColaEventos* cola);
+bool eventolisto(ColaEventos* cola, float tiempoActual);
+
+Evento extraeEvento(ColaEventos* cola);
+
+
+#endif
